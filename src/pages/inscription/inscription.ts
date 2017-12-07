622fb26e299cea003c4b949296059a2c44ea94aa
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2017-12-07 17:19:31 +0100

    Ajout de la vérification du mot de passe

--- src/pages/inscription/inscription.ts
+++ src/pages/inscription/inscription.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {Events, IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
 
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 /**
  * Generated class for the InscriptionPage page.
@@ -17,25 +17,25 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class InscriptionPage {
 
-  lastName: string;
-  firstName: string;
-  phone: string;
-  email: string;
-  profile: string;
-  //profile2: string;
-  profileType: any;
+  lastName:   string;
+  firstName:  string;
+  phone:      string;
+  email:      string;
+  profile:    string;
+  password:   string;
+  confirmPassword: string;
+  profileType: any; //Tableau contenant les types de profile
 
-  password: string;
+  lastNameCtrl:   FormControl;
+  firstNameCtrl:  FormControl;
+  phoneCtrl:      FormControl;
+  emailCtrl:      FormControl;
+  profileCtrl:    FormControl;
+  passwordCtrl:   FormControl;
+  confirmPasswordCtrl: FormControl;
 
-  lastNameCtrl: FormControl;
-  firstNameCtrl: FormControl;
-  phoneCtrl: FormControl;
-  emailCtrl: FormControl;
-  profileCtrl: FormControl;
-  passwordCtrl: FormControl;
-  userForm: FormGroup;
-
-  //profile2Ctrl: FormControl;
+  userForm:     FormGroup;
+  passwordForm: FormGroup;
 
   constructor(fb: FormBuilder, private toastCtrl: ToastController, public navCtrl : NavController,
               public events: Events) {
@@ -45,18 +45,14 @@ export class InscriptionPage {
     this.phoneCtrl = fb.control('', [Validators.minLength(10), Validators.maxLength(10), Validators.required]);
     this.emailCtrl = fb.control('', [Validators.email, Validators.required]);
     this.profileCtrl = fb.control('', Validators.required);
-    this.passwordCtrl = fb.control('', [ Validators.minLength(4), Validators.maxLength(4), Validators.required]);
-    //TODO password verification
 
-    this.profile = 'senior';
-    this.profileType = [
+    //TODO récupérer la liste de types de profile via le webservices
+    this.profile = 'senior';  //Valeur par défaut
+    this.profileType = [      //text= ce qui est affiché, value= la "vraie" valeur
       { text: 'Doctor', value: 'doctor' },
       { text: 'Senior', value: 'senior' },
       { text: 'Family', value: 'family' },
     ];
-
-    //Test:
-    //this.profileCtrl = fb.control('', Validators.required);
 
     this.userForm = fb.group({
       lastName: this.lastNameCtrl,
@@ -64,24 +60,37 @@ export class InscriptionPage {
       phone: this.phoneCtrl,
       email: this.emailCtrl,
       profile: this.profileCtrl,
-      password: this.passwordCtrl,
-      //profile2: this.profile2Ctrl
     });
+
+    this.passwordCtrl = fb.control('', [Validators.minLength(4), Validators.maxLength(4), Validators.required]);
+    this.confirmPasswordCtrl = fb.control('', [Validators.required,]);
+
+    this.passwordForm = fb.group({
+      password: this.passwordCtrl,
+      confirmPassword: this.confirmPasswordCtrl
+    }, {/*validator: this.matchingPasswords('password', 'confirmPassword')*/});
   }
 
   handleSubmit() {
-    let toast = this.toastCtrl.create({
-      message: 'Vous etes inscrit! Bien joué!',
-      duration: 3000,
-      position: 'bottom'
-    });
-    toast.present();
+    if (this.password == this.confirmPassword) {
+      let toast = this.toastCtrl.create({
+        message: 'Vous etes inscrit! Bien joué!',
+        duration: 3000,
+        position: 'bottom'
+      });
+      toast.present();
+    } else {
+      let toast = this.toastCtrl.create({
+        message: 'The confirm password is incorrect',
+        duration: 3000,
+        position: 'bottom'
+      });
+      toast.present();
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InscriptionPage');
-
-
   }
 
   testButton() {
@@ -95,4 +104,14 @@ export class InscriptionPage {
     console.log('Profile = '+this.profile);
   }
 
+  //Mon propre validator!! (inutilisable pour le moment)
+  static passwordsMatch(cg: FormGroup): {[err: string]: any} {
+    let pwd1 = cg.get('password');
+    let pwd2 = cg.get('confirmPassword');
+    let rv: {[error: string]: any} = {};
+    if ((pwd1.touched || pwd2.touched) && pwd1.value !== pwd2.value) {
+      rv['passwordMismatch'] = true;
+    }
+    return rv;
+  }
 }
