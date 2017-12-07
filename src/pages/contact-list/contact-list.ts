a26ae42f4b1b3268778b356ea92ef2ab1e681b7c
El Thomas <thomas.folmer@outlook.fr>
2017-12-07 14:25:25 +0100

    some call to the network were placed in the right place and the navigation to inscription page has been cleaned

--- src/pages/contact-list/contact-list.ts
+++ src/pages/contact-list/contact-list.ts
@@ -3,12 +3,9 @@ import {IonicPage, MenuController, NavController, NavParams} from 'ionic-angular
 
 import { EditContactPage } from '../edit-contact/edit-contact';
 import { ContactDetailPage } from '../contact-detail/contact-detail';
-/**
- * Generated class for the ContactListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {ContactServicesProvider} from "../../providers/contact-services/contact-services";
+
+
 
 @IonicPage()
 @Component({
@@ -17,12 +14,16 @@ import { ContactDetailPage } from '../contact-detail/contact-detail';
 })
 export class ContactListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menuCtrl: MenuController, public contactServices: ContactServicesProvider) {
     this.menuCtrl.enable(true);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactListPage');
+    // METTRE LE VRAI TOKEN
+    this.contactServices.getContacts("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZSI6IjY2NjY2NjY2NjYiLCJpYXQiOjE1MTI2NTIzOTIsImV4cCI6MTUxMjY1MjY5Mn0.ZG-mi3_vdDvnHeYyE9HUMg8al8UTBO0demeaHVUC_QM")
+      .then( data => {
+        console.log(data)
+      })
   }
   goToEditView(){
     this.navCtrl.push(EditContactPage);
